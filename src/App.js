d9339f947af74ce95732873270e25453ec81918b
matheussbarreto <matheus.santos.barreto@gmail.com>
2025-01-24 20:27:51 -0300

    cleanup: create react app and cleaning archives

---
+++ src/App.js
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Bem vindo ao Dashboard
+        </p>
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
